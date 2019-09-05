b669e1b9e8ee3aea675b5243df10663246d69fed
emir burgazli <emirhanburgazli@gmail.com>
2019-09-05 15:47:07 +0300

    commit

---
+++ asp_ImageProsess/asp_ImageProsess/UploadImage.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UploadImage.aspx.cs" Inherits="asp_ImageProsess.UploadImage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:FileUpload  runat="server" ID="fileUpload"/>
+        <br />
+        <br />
+        <asp:Button runat="server" ID="btn_save" Text="SAVE" OnClick="btn_save_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ asp_ImageProsess/asp_ImageProsess/UploadImage.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace asp_ImageProsess
+{
+    public partial class UploadImage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_save_Click(object sender, EventArgs e)
+        {
+            if (fileUpload.HasFile)
+            {
+                if (fileUpload.PostedFile.ContentType.Contains("image"))
+                {
+                    string guid = Guid.NewGuid().ToString();//ne işe yarar bak.
+                    string path = " ~/Pictures/" + guid + "." + fileUpload.PostedFile.ContentType.Split('/')[1];
+                    fileUpload.SaveAs(Server.MapPath(path));
+
+                    MemoryStream stream = new MemoryStream();
+                    System.Drawing.Image image = System.Drawing.Image.FromFile(Server.MapPath(path));
+                    image.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    byte[] byteArray = stream.ToArray();
+
+                    İmageProsessEntities db = new İmageProsessEntities();
+                    Images saveImage = new Images
+                    {
+                        ImageFile = byteArray,
+                        ImageFilePath = path
+                    };
+                    db.Images.Add(saveImage);
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
